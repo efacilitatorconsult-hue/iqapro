d69bf7605fd116864f7eb18bc457fe5ba4e849c5
MilestoneNefacilitatorconsult <efacilitatorconsult@gmail.com>
2026-04-08 16:34:40 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles.css'
+// 1. ADD THIS LINE
+import { SpeedInsights } from "@vercel/speed-insights/react" 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+    {/* 2. ADD THIS TAG */}
+    <SpeedInsights /> 
+  </React.StrictMode>,
+)
